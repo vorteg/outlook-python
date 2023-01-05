0854cd98acc05fd417d05807901e947f502274be
vic-fromWinds <vorteg.r@gmail.com>
2023-01-05 16:17:55 -0600

    MVP 1.0 WORKING

--- emails.py
+++ emails.py
@@ -1,16 +1,19 @@
-import win32apicom.client as client
+import win32com.client as client
 from typing import List
+from pathlib import Path
 
 
 def send_email(
-    to_email: str, body: str, path_atachments: List[str], subject: str = "Prueba"
+    to_email: str, body: str, path_atachments: List[str], subject: str = "IBM Balance"
 ) -> None:
     outlook = client.Dispatch("Outlook.Application")
     message = outlook.CreateItem(0)
+    message.Display()
     message.to = to_email
     message.Subject = subject
     message.HTMLBody = body
     for i in path_atachments:
-        message.Attachements.Add(i)
-    message.save()
-    message.send()
+        message.Attachments.Add(i)
+    
+    message.Save()
+    message.Send()
